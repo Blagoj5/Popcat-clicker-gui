32fed41f46137a3813f51fecf4c0a64001788aa2
Blagoj <blagoj.petrov5@gmail.com>
2021-04-14 04:32:02 +0200

    Making progress

--- dist/popClickScript.js
+++ dist/popClickScript.js
@@ -39,77 +39,165 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.popScript = void 0;
+exports.Clicker = void 0;
 var puppeteer_1 = __importDefault(require("puppeteer"));
-var popScript = function (headless) {
-    if (headless === void 0) { headless = false; }
-    return __awaiter(void 0, void 0, void 0, function () {
-        var browser, page, startDate, span, i, finishDate, text, timesClicked, clicks;
-        return __generator(this, function (_a) {
-            switch (_a.label) {
-                case 0: return [4, puppeteer_1.default.launch({
-                        headless: headless,
-                    })];
-                case 1:
-                    browser = _a.sent();
-                    return [4, browser.newPage()];
-                case 2:
-                    page = _a.sent();
-                    return [4, page.goto('https://popcat.click/')];
-                case 3:
-                    _a.sent();
-                    startDate = document.getElementById('startDate');
-                    if (startDate) {
-                        span = document.createElement('span');
-                        span.textContent = " " + new Date().toString();
-                        startDate.appendChild(span);
-                        startDate.style.display = 'block';
-                    }
-                    i = 0;
-                    _a.label = 4;
-                case 4:
-                    if (!(i < 100)) return [3, 10];
-                    return [4, page.keyboard.press('a')];
-                case 5:
-                    _a.sent();
-                    return [4, page.keyboard.press('d')];
-                case 6:
-                    _a.sent();
-                    return [4, page.keyboard.press('w')];
-                case 7:
-                    _a.sent();
-                    return [4, page.keyboard.press('g')];
-                case 8:
-                    _a.sent();
-                    return [4, page.keyboard.press('p')];
-                case 9:
-                    _a.sent();
-                    i += 1;
-                    console.log('Clicked');
-                    return [3, 4];
-                case 10:
-                    finishDate = document.getElementById('finishDate');
-                    if (finishDate) {
-                        text = document.createTextNode(" " + new Date().toString());
-                        finishDate.appendChild(text);
-                        finishDate.style.display = 'block';
-                    }
-                    timesClicked = document.getElementById('timesClicked');
-                    if (timesClicked) {
-                        clicks = document.createTextNode((i * 5).toString());
-                        timesClicked.appendChild(clicks);
-                        timesClicked.style.display = 'block';
-                    }
-                    return [4, page.screenshot({ path: 'dokaz.png' })];
-                case 11:
-                    _a.sent();
-                    return [4, browser.close()];
-                case 12:
-                    _a.sent();
-                    return [2];
-            }
+var Clicker = (function () {
+    function Clicker() {
+        this.clickedTimes = 0;
+    }
+    Clicker.prototype.initScript = function (headless) {
+        return __awaiter(this, void 0, void 0, function () {
+            var _a, howMany, pages, index, page, startDate, span, i, index, page;
+            return __generator(this, function (_b) {
+                switch (_b.label) {
+                    case 0:
+                        this.clickedTimes = 0;
+                        _a = this;
+                        return [4, puppeteer_1.default.launch({
+                                headless: headless,
+                            })];
+                    case 1:
+                        _a.browser = _b.sent();
+                        howMany = document.getElementById('select');
+                        pages = [];
+                        index = 0;
+                        _b.label = 2;
+                    case 2:
+                        if (!(index < +howMany.value)) return [3, 6];
+                        return [4, this.browser.newPage()];
+                    case 3:
+                        page = _b.sent();
+                        return [4, page.goto('https://popcat.click/')];
+                    case 4:
+                        _b.sent();
+                        pages.push(page);
+                        _b.label = 5;
+                    case 5:
+                        index++;
+                        return [3, 2];
+                    case 6:
+                        startDate = document.getElementById('startDate');
+                        if (startDate) {
+                            span = document.createElement('span');
+                            span.textContent = " " + new Date().toString();
+                            startDate.appendChild(span);
+                            startDate.style.display = 'block';
+                        }
+                        i = 0;
+                        _b.label = 7;
+                    case 7:
+                        if (!(i < 100)) return [3, 16];
+                        index = 0;
+                        _b.label = 8;
+                    case 8:
+                        if (!(index < pages.length)) return [3, 15];
+                        page = pages[index];
+                        console.log('page');
+                        return [4, page.keyboard.press('a')];
+                    case 9:
+                        _b.sent();
+                        return [4, page.keyboard.press('d')];
+                    case 10:
+                        _b.sent();
+                        return [4, page.keyboard.press('w')];
+                    case 11:
+                        _b.sent();
+                        return [4, page.keyboard.press('g')];
+                    case 12:
+                        _b.sent();
+                        return [4, page.keyboard.press('p')];
+                    case 13:
+                        _b.sent();
+                        this.clickedTimes += 5;
+                        _b.label = 14;
+                    case 14:
+                        index++;
+                        return [3, 8];
+                    case 15:
+                        i += 1;
+                        console.log('Clicked');
+                        return [3, 7];
+                    case 16: return [2];
+                }
+            });
         });
-    });
-};
-exports.popScript = popScript;
+    };
+    Clicker.prototype.exit = function () {
+        return __awaiter(this, void 0, void 0, function () {
+            var finishDate, text, timesClicked, clicks;
+            return __generator(this, function (_a) {
+                switch (_a.label) {
+                    case 0:
+                        if (!this.browser) return [3, 2];
+                        finishDate = document.getElementById('finishDate');
+                        if (finishDate) {
+                            text = document.createTextNode(" " + new Date().toString());
+                            finishDate.appendChild(text);
+                            finishDate.style.display = 'block';
+                        }
+                        timesClicked = document.getElementById('timesClicked');
+                        if (timesClicked) {
+                            clicks = document.createTextNode(this.clickedTimes.toString());
+                            timesClicked.appendChild(clicks);
+                            timesClicked.style.display = 'block';
+                        }
+                        return [4, this.browser.close()];
+                    case 1:
+                        _a.sent();
+                        _a.label = 2;
+                    case 2: return [2];
+                }
+            });
+        });
+    };
+    Clicker.prototype.initScriptWithMultiplePages = function (headless) {
+        return __awaiter(this, void 0, void 0, function () {
+            var _a, howMany, startDate, span, browsers, index;
+            var _this = this;
+            return __generator(this, function (_b) {
+                switch (_b.label) {
+                    case 0:
+                        this.headless = headless;
+                        _a = this;
+                        return [4, puppeteer_1.default.launch({
+                                headless: false,
+                            })];
+                    case 1:
+                        _a.browser = _b.sent();
+                        this.clickedTimes = 0;
+                        howMany = document.getElementById('select');
+                        startDate = document.getElementById('startDate');
+                        if (startDate) {
+                            span = document.createElement('span');
+                            span.textContent = " " + new Date().toString();
+                            startDate.appendChild(span);
+                            startDate.style.display = 'block';
+                        }
+                        browsers = [];
+                        for (index = 0; index < +howMany.value; index++) {
+                            browsers.push(new Promise(function () { return __awaiter(_this, void 0, void 0, function () {
+                                return __generator(this, function (_a) {
+                                    switch (_a.label) {
+                                        case 0: return [4, this.initScript(false)];
+                                        case 1:
+                                            _a.sent();
+                                            return [2];
+                                    }
+                                });
+                            }); }));
+                        }
+                        return [4, Promise.all(browsers)];
+                    case 2:
+                        _b.sent();
+                        return [4, this.exit()];
+                    case 3:
+                        _b.sent();
+                        return [2];
+                }
+            });
+        });
+    };
+    return Clicker;
+}());
+exports.Clicker = Clicker;
 //# sourceMappingURL=popClickScript.js.map
--- src/popClickScript.ts
+++ src/popClickScript.ts
@@ -1,43 +1,112 @@
 import puppeteer from 'puppeteer';
+import { Browser } from 'puppeteer';
+import { Page } from 'puppeteer';
 
-// TODO: ADD FORCE QUIT BTN, ADD MULTIPLE TABS OPTION
-export const popScript = async (headless: boolean = false) => {
-  const browser = await puppeteer.launch({
-    headless,
-  });
-  const page = await browser.newPage();
-  await page.goto('https://popcat.click/');
-  //   const startDate = document.querySelector('#startDate');
-  const startDate = document.getElementById('startDate');
-  if (startDate) {
-    const span = document.createElement('span');
-    span.textContent = ` ${new Date().toString()}`;
-    startDate.appendChild(span);
-    startDate.style.display = 'block';
-  }
-  let i = 0;
-  while (i < 100) {
-    await page.keyboard.press('a');
-    await page.keyboard.press('d');
-    await page.keyboard.press('w');
-    await page.keyboard.press('g');
-    await page.keyboard.press('p');
-    i += 1;
-    console.log('Clicked');
-  }
-  const finishDate = document.getElementById('finishDate');
-  if (finishDate) {
-    const text = document.createTextNode(` ${new Date().toString()}`);
-    finishDate.appendChild(text);
-    finishDate.style.display = 'block';
+export class Clicker {
+  page: Page;
+  browser: Browser;
+  headless: boolean;
+  clickedTimes: number = 0;
+
+  async initScript(headless: boolean) {
+    this.clickedTimes = 0;
+    this.browser = await puppeteer.launch({
+      headless: headless,
+    });
+
+    const howMany = document.getElementById('select');
+
+    const pages: Page[] = [];
+    // Create pages
+    for (
+      let index = 0;
+      index < +(howMany as HTMLSelectElement).value;
+      index++
+    ) {
+      const page = await this.browser.newPage();
+      await page.goto('https://popcat.click/');
+      pages.push(page);
+    }
+
+    const startDate = document.getElementById('startDate');
+    if (startDate) {
+      const span = document.createElement('span');
+      span.textContent = ` ${new Date().toString()}`;
+      startDate.appendChild(span);
+      startDate.style.display = 'block';
+    }
+
+    let i = 0;
+    while (i < 100) {
+      for (let index = 0; index < pages.length; index++) {
+        const page = pages[index];
+        console.log('page');
+
+        await page.keyboard.press('a');
+        await page.keyboard.press('d');
+        await page.keyboard.press('w');
+        await page.keyboard.press('g');
+        await page.keyboard.press('p');
+        this.clickedTimes += 5;
+      }
+      i += 1;
+      console.log('Clicked');
+    }
   }
 
-  const timesClicked = document.getElementById('timesClicked');
-  if (timesClicked) {
-    const clicks = document.createTextNode((i * 5).toString());
-    timesClicked.appendChild(clicks);
-    timesClicked.style.display = 'block';
+  async exit() {
+    if (this.browser) {
+      const finishDate = document.getElementById('finishDate');
+      if (finishDate) {
+        const text = document.createTextNode(` ${new Date().toString()}`);
+        finishDate.appendChild(text);
+        finishDate.style.display = 'block';
+      }
+
+      const timesClicked = document.getElementById('timesClicked');
+      if (timesClicked) {
+        const clicks = document.createTextNode(this.clickedTimes.toString());
+        timesClicked.appendChild(clicks);
+        timesClicked.style.display = 'block';
+      }
+      await this.browser.close();
+    }
   }
-  await page.screenshot({ path: 'dokaz.png' });
-  await browser.close();
-};
+
+  // TODO: test better way
+  async initScriptWithMultiplePages(headless: boolean) {
+    this.headless = headless;
+    this.browser = await puppeteer.launch({
+      headless: false,
+    });
+    this.clickedTimes = 0;
+
+    const howMany = document.getElementById('select');
+
+    const startDate = document.getElementById('startDate');
+    if (startDate) {
+      const span = document.createElement('span');
+      span.textContent = ` ${new Date().toString()}`;
+      startDate.appendChild(span);
+      startDate.style.display = 'block';
+    }
+
+    const browsers: Promise<any>[] = [];
+    for (
+      let index = 0;
+      index < +(howMany as HTMLSelectElement).value;
+      index++
+    ) {
+      browsers.push(
+        new Promise(async () => {
+          await this.initScript(false);
+        })
+      );
+    }
+    await Promise.all(browsers);
+
+    await this.exit();
+  }
+}
+
+// TODO: Fix the spamming of buttons and text
